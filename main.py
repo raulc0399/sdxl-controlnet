c2b9071dcb952c1164d3652e5bbdc2d72587f3b2
raulc <raulciotescu@gmail.com>
2024-01-26 21:11:35 +0100

    refactor canny processor

--- main.py
+++ main.py
@@ -14,16 +14,74 @@ import cv2
 import os
 from PIL import Image
 
+def pad64(x):
+    return int(np.ceil(float(x) / 64.0) * 64 - x)
+
+
+def safer_memory(x):
+    # Fix many MAC/AMD problems
+    return np.ascontiguousarray(x.copy()).copy()
+
 class ControlNetCannyProcessor:
+    @staticmethod
+    def HWC3(x):
+        assert x.dtype == np.uint8
+        if x.ndim == 2:
+            x = x[:, :, None]
+        assert x.ndim == 3
+        H, W, C = x.shape
+        assert C == 1 or C == 3 or C == 4
+        if C == 3:
+            return x
+        if C == 1:
+            return np.concatenate([x, x, x], axis=2)
+        if C == 4:
+            color = x[:, :, 0:3].astype(np.float32)
+            alpha = x[:, :, 3:4].astype(np.float32) / 255.0
+            y = color * alpha + 255.0 * (1.0 - alpha)
+            y = y.clip(0, 255).astype(np.uint8)
+            return y
+        
+    @staticmethod
+    def resize_image_with_pad(input_image, resolution, skip_hwc3=False):
+        if skip_hwc3:
+            img = input_image
+        else:
+            img = ControlNetCannyProcessor.HWC3(input_image)
+        H_raw, W_raw, _ = img.shape
+        k = float(resolution) / float(min(H_raw, W_raw))
+        interpolation = cv2.INTER_CUBIC if k > 1 else cv2.INTER_AREA
+        H_target = int(np.round(float(H_raw) * k))
+        W_target = int(np.round(float(W_raw) * k))
+        img = cv2.resize(img, (W_target, H_target), interpolation=interpolation)
+        H_pad, W_pad = pad64(H_target), pad64(W_target)
+        img_padded = np.pad(img, [[0, H_pad], [0, W_pad], [0, 0]], mode='edge')
+
+        def remove_pad(x):
+            return safer_memory(x[:H_target, :W_target])
+
+        return safer_memory(img_padded), remove_pad
+
+    @staticmethod
+    def canny(img, res=512, thr_a=100, thr_b=200, **kwargs):
+        l, h = thr_a, thr_b
+        img, remove_pad = ControlNetCannyProcessor.resize_image_with_pad(img, res)        
+        
+        result = cv2.Canny(img, l, h)
+
+        return remove_pad(result)
+
     @staticmethod
     def process(image_url):
         image = load_image(image_url)
 
         image = np.array(image)
-        image = cv2.Canny(image, 100, 200)
+        # image = 
 
-        image = image[:, :, None]
-        image = np.concatenate([image, image, image], axis=2)
+        # image = image[:, :, None]
+        # image = np.concatenate([image, image, image], axis=2)
+
+        image = ControlNetCannyProcessor.canny(image, 512, 75, 250)
 
         canny_image = Image.fromarray(image)
         return canny_image
@@ -57,7 +115,7 @@ class DiffusionRunner:
             torch_dtype=torch.float16,
             # variant="fp16",
             use_safetensors=True,
-        )#.to("cuda")
+        )
 
     # def load_vae(self):
         # self.vae = AutoencoderKL.from_single_file(
@@ -65,7 +123,7 @@ class DiffusionRunner:
         #     torch_dtype=torch.float16,
         #     variant="fp16",
         #     use_safetensors=True,
-        # ).to("cuda")
+        # )
 
     def load_refiner(self):
         self.refiner = StableDiffusionXLImg2ImgPipeline.from_single_file(
