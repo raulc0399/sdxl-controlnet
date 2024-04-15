1eaaf59263848589c1f2df37fa9d6ef6dbce5b29
raulc <raulciotescu@gmail.com>
2024-04-15 10:15:44 +0200

    print current params

--- main.py
+++ main.py
@@ -304,6 +304,7 @@ def run_diffusion_experiments(diffusion_runner, model, control_image_url, contro
             for num_inference_steps in num_inference_steps_vals:
                 for guidance_scale in guidance_scale_vals:
                     print(f"Running experiment {experiment_no} from {all_experiments_count} experiments")
+                    print(f"Model: {model}, Controlnet Conditioning Scale: {controlnet_conditioning_scale}, Num Inference Steps: {num_inference_steps}, Guidance Scale: {guidance_scale}, Prompt: {prompt:.20}")
 
                     image, upscaled_image = diffusion_runner.run(
                         prompt,
@@ -315,11 +316,11 @@ def run_diffusion_experiments(diffusion_runner, model, control_image_url, contro
                     
                     params = {
                         "model": model,
-                        "prompt": prompt,
                         "control_image_url": control_image_url,
                         "controlnet_conditioning_scale": controlnet_conditioning_scale,
                         "num_inference_steps": num_inference_steps,
                         "guidance_scale": guidance_scale,
+                        "prompt": prompt,
                     }
 
                     ImageUtils.save_image_with_timestamp(image, params=params)
@@ -329,7 +330,8 @@ def run_diffusion_experiments(diffusion_runner, model, control_image_url, contro
 
                     experiment_no += 1
 
-                    exit()
+                    if experiment_no == 10:
+                        exit()
 
 if __name__ == "__main__":
     CONTROL_IMAGE_URL = "/home/raul/codelab/objs/obj4/4j.jpg"
