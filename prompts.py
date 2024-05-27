99f7cfc9feba433016264b6d46badb9b395e8b83
raci0399 <raci0399@gmail.com>
2024-05-27 18:20:33 +0200

    negative prompt

--- prompts.py
+++ prompts.py
@@ -1,15 +1,54 @@
 prompts = [
-    # "A realistic image of a modern house in the suburbs of a modern city, showcasing a unique blend of classic architecture with contemporary elements. The house is not on the main street, surrounded by a variety of vegetation. It should display features typical of traditional German houses, such as steep gabled roofs or timber framing, integrated with modern design aspects like geometric (quadratic) shapes and large glass panels. Vary the angle of the image for a different perspective, and alter the sun's position to change the lighting, creating distinctive shadows and highlights. The surrounding environment should have diverse trees and shrubs, reinforcing the house's connection with nature. The scene is captured on a sunny day to accentuate the fusion of architectural styles",
-    # "a 3d rendering of a row of houses with realistic staircase between the floors, sunny, white exterior, warm day, modern city suburb",
-    # "Architecture photography of a row of houses with a staircase between the floors, sunny, white exterior, warm day, modern city",
-    # "Hyperdetailed photography of a row of houses with a staircase between the floors, sunny, white exterior, warm day, modern city",
-    # "RAW photo of house in german suburb, nice warm, day, sunny, white exterior",
-    # "architectural rendering of houses of same design in modern city suburb, stairs between the levels, nice warm, day, sunny, white exterior",
-    # "A 3d rendering of a modern house in the suburbs of a modern city. Stairs between the floors. The house is not on the main street, nice clean vegetation. sun's position in the morning, on a warm sunny day."
-    # "sun, sunlight, sunny sky, real estate, photot, blue sky, best quality, high resolution, high quality, highres, analog photo, high detailed textures"
-    "nice houses on a sunny day, modern architecture, nice modern garden",
-    "nice houses on a sunny day, modern architecture, front lawn",
-    "a beautiful modern houses, in a modern city, (sharp focus:1.2), extremely detailed, (photorealistic:1.4), (RAW image, 8k high resolution:1.2), RAW candid cinema, 16mm, color graded Portra 400 film, ultra realistic, cinematic film still, subsurface scattering, ray tracing, (volumetric lighting)",
-    "suburban house, dusk, modern architecture, two-story, landscaped, front lawn, exterior lighting, clear sky, twilight hues, residential area, paved road, high dynamic range, wide angle, curb appeal, stone accents, window reflections, tranquil scene",
-    "suburban house, modern architecture, two-story, landscaped, front lawn, sun, beautiful day, clear sky, residential area, paved road, high dynamic range, wide angle, window reflections, tranquil scene"
+    [
+        "A realistic image of a modern house in the suburbs of a modern city, showcasing a unique blend of classic architecture with contemporary elements. The house is not on the main street, surrounded by a variety of vegetation. It should display features typical of traditional German houses, such as steep gabled roofs or timber framing, integrated with modern design aspects like geometric (quadratic) shapes and large glass panels. Vary the angle of the image for a different perspective, and alter the sun's position to change the lighting, creating distinctive shadows and highlights. The surrounding environment should have diverse trees and shrubs, reinforcing the house's connection with nature. The scene is captured on a sunny day to accentuate the fusion of architectural styles",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "a 3d rendering of a row of houses with realistic staircase between the floors, sunny, white exterior, warm day, modern city suburb",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "Architecture photography of a row of houses with a staircase between the floors, sunny, white exterior, warm day, modern city",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "Hyperdetailed photography of a row of houses with a staircase between the floors, sunny, white exterior, warm day, modern city",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "RAW photo of house in german suburb, nice warm, day, sunny, white exterior",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "architectural rendering of houses of same design in modern city suburb, stairs between the levels, nice warm, day, sunny, white exterior",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "A 3d rendering of a modern house in the suburbs of a modern city. Stairs between the floors. The house is not on the main street, nice clean vegetation. sun's position in the morning, on a warm sunny day.",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "sun, sunlight, sunny sky, real estate, photot, blue sky, best quality, high resolution, high quality, highres, analog photo, high detailed textures",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "nice houses on a sunny day, modern architecture, nice modern garden",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark"
+    ],
+    [
+        "nice houses on a sunny day, modern architecture, front lawn",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark"
+    ],
+    [
+        "a beautiful modern houses, in a modern city, (sharp focus:1.2), extremely detailed, (photorealistic:1.4), (RAW image, 8k high resolution:1.2), RAW candid cinema, 16mm, color graded Portra 400 film, ultra realistic, cinematic film still, subsurface scattering, ray tracing, (volumetric lighting)",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "suburban house, dusk, modern architecture, two-story, landscaped, front lawn, exterior lighting, clear sky, twilight hues, residential area, paved road, high dynamic range, wide angle, curb appeal, stone accents, window reflections, tranquil scene",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
+    [
+        "suburban house, modern architecture, two-story, landscaped, front lawn, sun, beautiful day, clear sky, residential area, paved road, high dynamic range, wide angle, window reflections, tranquil scene",
+        "sketch, unclear, low quality, ugly, fantasy, undetailed,  cgi, airbrushed, plastic, deformed, watermark",
+    ],
 ]
