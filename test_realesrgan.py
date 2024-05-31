cf478f4ad02b632dc03e583762f3a0ae3776c6c4
raci0399 <raci0399@gmail.com>
2024-05-31 18:45:00 +0200

    using tiling

--- test_realesrgan.py
+++ test_realesrgan.py
@@ -6,9 +6,9 @@ input_dir = "./input_imgs"
 output_dir = "./gen_imgs"
 
 model_dir = "/home/raul/codelab/models/upscalers"
-scale = 4
 
-model_names = ["4x_NMKD-Siax_200k", "DF2K", "4x-UltraSharp"]
+# model_names = ["4x_NMKD-Siax_200k", "DF2K", "4x-UltraSharp"]
+model_names = ["4x-UltraSharp"]
 
 for model_name in model_names:
     print(f"Upscaling with {model_name}")
@@ -21,7 +21,8 @@ for model_name in model_names:
 
             input_image = load_image(os.path.join(input_dir, filename))
             
-            img = upscaler.do_upscale(input_image)
+            # based on a1111 default values ESRGAN_tile and ESRGAN_tile_overlap
+            img = upscaler.do_upscale(input_image, tile_size=192, tile_overlap=8)
                 
             output_filename = f"image_esrgan_{model_name}_{filename}"
             img.save(os.path.join(output_dir, output_filename))
