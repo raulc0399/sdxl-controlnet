c6ee5e2a38284222d79e0797f908b9b287a390a1
raci0399 <raci0399@gmail.com>
2024-05-31 16:40:20 +0200

    iterate over images also using 4x-UltraSharp

--- test_esrgan.py
+++ test_esrgan.py
@@ -1,21 +1,27 @@
+import os
 from upscaler.esrgan_model import UpscalerESRGAN
 from diffusers.utils import load_image
 
-if __name__ == "__main__":
-    input_image = load_image(r"D:\raul\stuff\try\gen_imgs\20240203-191525-generated.jpg")
+input_dir = "./input_imgs"
+output_dir = "./gen_imgs"
 
-    model_dir = r"D:\raul\models\upscalers"
-    scale = 4
+model_dir = "/home/raul/codelab/models/upscalers"
+scale = 4
 
-    model_name = "4x_NMKD-Siax_200k"
+model_names = ["4x_NMKD-Siax_200k", "DF2K", "4x-UltraSharp"]
+
+for model_name in model_names:
+    print(f"Upscaling with {model_name}")
+
     upscaler = UpscalerESRGAN(model_name, model_dir, scale)
-    img = upscaler.do_upscale(input_image)
-    
-    img.save(rf"D:\raul\stuff\try\gen_imgs\image_esrgan_{model_name}.jpg")    
 
-    model_name = "DF2K"
-    upscaler = UpscalerESRGAN(model_name, model_dir, scale)
-    img = upscaler.do_upscale(input_image)
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            print(f"\tUpscaling {filename}")
 
-    img.save(rf"D:\raul\stuff\try\gen_imgs\image_esrgan_{model_name}.jpg")    
-    
+            input_image = load_image(os.path.join(input_dir, filename))
+            
+            img = upscaler.do_upscale(input_image)
+                
+            output_filename = f"image_esrgan_{model_name}_{filename}"
+            img.save(os.path.join(output_dir, output_filename))
